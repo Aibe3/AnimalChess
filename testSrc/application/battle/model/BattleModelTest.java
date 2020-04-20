28b99d6ff608524ab5b0057bc2137c3799466969
Aibe3 <aibenorio19930221@gmail.com>
2020-04-21 00:25:34 +0900

    疎通が完了したのでテストクラスを作成。 InaccessibleMemberUtilクラスとHistoryTestクラスは一旦実装完了。

---
+++ testSrc/application/battle/model/BattleModelTest.java
@@ -0,0 +1,31 @@
+package application.battle.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BattleModelTest {
+    
+    private BattleModel model = new BattleModel();
+    
+    @Test
+    void testInit() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    void testGetCanMovePoint() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    void testGetCanPopPoint() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    void testMovePiece() {
+        fail("Not yet implemented");
+    }
+    
+}
